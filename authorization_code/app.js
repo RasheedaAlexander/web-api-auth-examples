47ee99e6564de0f287f0349cccb42595618b9e94
Rasheeda Alexander <rasheedaalexander@Rasheedas-MBP.fios-router.home>
2020-03-04 15:24:14 -0500

    End user can authorize app to fetch data from their Spotify

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -13,9 +13,9 @@ var cors = require('cors');
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 
-var client_id = 'CLIENT_ID'; // Your client id
-var client_secret = 'CLIENT_SECRET'; // Your secret
-var redirect_uri = 'REDIRECT_URI'; // Your redirect uri
+var client_id = '77a9f22e16f8416ca179427af478ac90'; // Your client id
+var client_secret = '47852990a2aa4b3c964f7dbe24e2d269'; // Your secret
+var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
